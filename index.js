b047498fd569b63f3c5c9f27126f9f0257347443
CatherineHonors <cathonors@gmail.com>
2022-03-17 17:24:18 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// Code your solutions in this file
+function writeCards(namesForCards, event){
+    let messages = [];
+    for(let i = 0; i < namesForCards.length; i++){
+        messages.push(`Thank you, ${namesForCards[i]}, for the wonderful surprise gift!`);
+    }
+    return messages;
+}
+
+
+function countDown(downCounter) {
+    while(downCounter >= 0) {
+        console.log(downCounter);
+        downCounter--;
+    }
+}
